abb2267ca6bbbcb8900367618a1b28d6d40be815
Devin Walters <devinw@gmail.com>
2009-12-09 23:22:32 -0600

    fixing the name in the project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leiningen "1.0.0-SNAPSHOT"
+(defproject cljex "1.0.0-SNAPSHOT"
   :description "cljex provides examples for clojure's core api documentation"
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
