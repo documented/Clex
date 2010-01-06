f18986741c98d33ba85619d9b83e22f04acc91a0
Devin Walters <devinw@gmail.com>
2010-01-06 14:04:38 -0600

    cleanup

--- src/cljex/markdown.clj
+++
@@ -1,7 +0,0 @@
-(ns cljex.markdown
-  [:use compojure,
-   clojure.contrib.duck-streams,
-   clojure.contrib.str-utils,
-   clojure.contrib.shell-out])
-
-;; pygmentize -S manni -f html > pygments.css
