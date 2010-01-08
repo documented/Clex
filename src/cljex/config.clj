a6a1da4b14db850dd2482f076be64ca5ccd6ea8d
Devin Walters <devinw@gmail.com>
2010-01-08 10:59:14 -0600

    fixed problem with path.  it is required that you run the cljex.jar from the project's root directory.

--- src/cljex/config.clj
+++ src/cljex/config.clj
@@ -4,10 +4,10 @@
                                         ; Configuration Options
                                         ;==============================
 (def *markdown-command* "/usr/local/bin/markdown")
-(def *project-root* "/Users/defn/git/cljex/")
-(def *public-dir* (str *project-root* "src/cljex/public/"))
-(def *core-docs* (str *project-root* "src/docs/core-docs/"))
-(def *examples-dir* (str *project-root* "src/docs/examples/"))
+(def *project-root* (System/getProperty "user.dir"))
+(def *public-dir* (str *project-root* "/src/cljex/public/"))
+(def *core-docs* (str *project-root* "/src/docs/core-docs/"))
+(def *examples-dir* (str *project-root* "/src/docs/examples/"))
 
 (def *site-url* "http://localhost:8080")
 (def *site-title* "cljex")
